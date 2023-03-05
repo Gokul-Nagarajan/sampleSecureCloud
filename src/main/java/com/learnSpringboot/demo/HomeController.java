1dc6a365bba29bf7db90bd68e66dce47aeb5de22
Gokul Nagarajan <gokulnagarajang@gmail.com>
2023-03-05 16:42:12 +0530

    cheking thymeleaf dependency

---
+++ src/main/java/com/learnSpringboot/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.learnSpringboot.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/login")
+    public String hello() {
+        return "index";
+    }
+}
